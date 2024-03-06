fae2773f818e94f39d1c9cd1bb76c862c9c529fa
alejandro <alejandro.villamil@correounivalle.edu.co>
2024-03-06 15:45:37 -0500

    Animaciones a las figuras actualizado

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -32,25 +32,25 @@ export const Experience = () => {
             boxRef2.current.position.y = cosValue * 3;
         }        
         if (boxRef3.current) {
-            boxRef3.current.position.y = cosValue * 4;
+            boxRef3.current.position.y = sinValue * 4;
         }        
         if (boxRef4.current) {
             boxRef4.current.position.y = cosValue * 5;
         }        
         if (boxRef5.current) {
-            boxRef5.current.position.y = cosValue * 6;
+            boxRef5.current.position.y = sinValue * 6;
         }        
         if (boxRef6.current) {
             boxRef6.current.position.x = cosValue * 1;
         }        
         if (boxRef7.current) {
-            boxRef7.current.position.x = cosValue * 2;
+            boxRef7.current.position.x = sinValue * 2;
         }        
         if (boxRef8.current) {
             boxRef8.current.position.x = cosValue * 3;
         }        
         if (boxRef9.current) {
-            boxRef9.current.position.x = cosValue * 4;
+            boxRef9.current.position.x = sinValue * 4;
         }        
         if (boxRef10.current) {
             boxRef10.current.position.x = cosValue * 5;

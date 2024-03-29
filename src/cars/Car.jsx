f14349506fc480828204843a9912d964d6cd0e47
Sebastian Rey <reysebastian77@gmail.com>
2024-03-29 15:16:48 -0500

    ajuste de config en vite, implementacion de carrito

--- src/cars/Car.jsx
+++ src/cars/Car.jsx
@@ -1,8 +1,27 @@
 import { useGLTF } from "@react-three/drei"
-import supra from '../assets/models/cars/porche.glb'
 
-export const Car = () => {  
-    const carmodel = useGLTF("/src/assets/models/cars/porche.glb")
-    console.log(carmodel)
-    return carmodel
+export const Car = (props) => {
+    const { nodes, materials } = useGLTF('/src/assets/models/cars/porche.glb')
+
+    console.log(nodes, "nodes")
+    console.log(materials,"materiales")
+
+    return (
+        <group {...props} dispose={null}>
+            <group>
+                <group>
+                    <mesh geometry={nodes.Circle001.geometry} material={materials.car} />
+                    <mesh geometry={nodes.Circle001_1.geometry} material={materials.dark_metal} />
+                    <mesh geometry={nodes.Circle001_2.geometry} material={materials.plastic} />
+                    <mesh geometry={nodes.Circle001_3.geometry} material={materials.chrome} />
+                    <mesh geometry={nodes.Circle001_4.geometry} material={materials.windows} />
+                    <mesh geometry={nodes.Circle001_5.geometry} material={materials.silencer} />
+                    <mesh geometry={nodes.Circle001_6.geometry} material={materials.car_headlights} />
+                    <mesh geometry={nodes.Circle001_7.geometry} material={materials.turn_signals} />
+                    <mesh geometry={nodes.Circle001_8.geometry} material={materials.stop_signals} />
+                    <mesh geometry={nodes.Circle001_9.geometry} material={materials.reverse} />
+                </group>
+            </group>
+        </group>
+    )
 }

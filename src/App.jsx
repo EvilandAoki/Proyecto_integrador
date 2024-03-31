4faa891d49a1bf7610a5051175de7705c535f580
Sebastian Rey <reysebastian77@gmail.com>
2024-03-31 15:47:51 -0500

    implementacion de controles en el contexto global

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import { Experience } from "./components/Experience";
 import { Physics } from "@react-three/rapier";
 import { Suspense } from "react";
 import { KeyboardControls } from "./controls";
+import HomeScreen from "./layouts/HomeScreen";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [90, 20, 60], fov: 40 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Suspense>
-        <Physics>
-          <Experience />
-        </Physics>
-      </Suspense>
-      <KeyboardControls/>
-    </Canvas>
+    <HomeScreen>
+      <Canvas shadows camera={{ position: [90, 20, 60], fov: 40 }}>
+        <color attach="background" args={["#ececec"]} />
+        <Suspense>
+          <Physics>
+            <Experience />
+          </Physics>
+        </Suspense>
+        <KeyboardControls />
+      </Canvas>
+    </HomeScreen>
   );
 }
 

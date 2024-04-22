584d1ce6e594135f8e1aaeaa984cbb3268e41de0
sebastianR <reysebastian77@gmail.com>
2024-04-22 15:39:50 -0500

    ajuste de fisicas en el componente worldOne

--- src/pages/levelOne/world/WorldOne.jsx
+++ src/pages/levelOne/world/WorldOne.jsx
@@ -5,7 +5,7 @@ export const WorldOne = (props) => {
     const { nodes, materials } = useGLTF('/assets/models/maps/basemapa.glb')
     return (
         <group {...props} dispose={null}>
-            <RigidBody type="fixed" coliders="trimesh">
+            <RigidBody type="fixed" colliders="trimesh">
                 <group name="Scene">
                     <mesh name="walls" geometry={nodes.walls.geometry} material={materials.Material} />
                     <mesh name="floor" geometry={nodes.floor.geometry} material={materials.Material} />

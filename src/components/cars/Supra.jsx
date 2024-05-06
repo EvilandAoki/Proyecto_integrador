397ffcd65aed3ee02fee96731b709f0d4d462ec0
Sebastian Rey <reysebastian77@gmail.com>
2024-05-06 15:50:41 -0500

    implementacion de ruedas parte uno

--- src/components/cars/Supra.jsx
+++ src/components/cars/Supra.jsx
@@ -2,7 +2,8 @@ import { useRef, useEffect } from "react"
 import { useGLTF } from "@react-three/drei"
 import { RigidBody, quat } from "@react-three/rapier"
 import { useCarContext } from "../../context"
-import { useBox } from "@react-three/cannon"
+import { useBox, useRaycastVehicle } from "@react-three/cannon"
+import { useWheels } from "../../hooks/useWheels"
 
 
 const Supra = (props) => {
@@ -12,13 +13,13 @@ const Supra = (props) => {
   const supraBodyRef = useRef()
   const supraRef = useRef()
 
-  const position = [-1.5, 0.5, 20];
-  const width = 0.15;
-  const height = 0.07;
-  const front = 0.15;
+  const position = [-3, 0.5, 10];
+  const width = 2;
+  const height = 0.7;
+  const front = 4;
   const wheelRadius = 0.5;
 
-  const chassisBodyArgs = [width, height, front * 2];
+  const chassisBodyArgs = [width, height, front];
 
   const [chassisBody, chassisApi] = useBox(
     () => ({ args: chassisBodyArgs, mass: 150, position }),
@@ -26,49 +27,46 @@ const Supra = (props) => {
   )
 
 
-  // useEffect(() => {
-  //   setCar({
-  //     ref: supraRef.current,
-  //     body: supraBodyRef.current
-  //   })
-  // }, [supraBodyRef?.current, supraRef?.current])
+  const [wheels, wheelInfos] = useWheels(width, height, front, wheelRadius);
 
-  // console.log(nodes, "nodes")
-  // console.log(materials, "materiales")
+  const [vehicle, vehicleApi] = useRaycastVehicle(
+    () => ({
+      chassisBody,
+      wheelInfos,
+      wheels,
+    }),
+    useRef(null)
+  )
 
 
   return (<>
-    <group  {...props} ref={chassisBody} type="fixed"  dispose={null} >
+    <group ref={chassisBody}>
 
-      <group >
-        <group>
-          <mesh castShadow={true} geometry={nodes.car_1.geometry} material={materials.main_color} />
-          <mesh castShadow={true} geometry={nodes.car_2.geometry} material={materials.dark_plastic} />
-          <mesh castShadow={true} geometry={nodes.car_3.geometry} material={materials.back_ligths} />
-
+      <group  {...props} type="fixed" dispose={null} >
+        <group >
           <group>
-            <mesh castShadow={true} geometry={nodes.wheel_FL_1.geometry} material={materials.tire} />
-            <mesh castShadow={true} geometry={nodes.wheel_FL_2.geometry} material={materials.rims} />
+            <mesh castShadow={true} geometry={nodes.car_1.geometry} material={materials.main_color} />
+            <mesh castShadow={true} geometry={nodes.car_2.geometry} material={materials.dark_plastic} />
+            <mesh castShadow={true} geometry={nodes.car_3.geometry} material={materials.back_ligths} />
+            <group ref={wheels[0]}>
+              <mesh castShadow={true} geometry={nodes.wheel_FL_1.geometry} material={materials.tire} />
+              <mesh castShadow={true} geometry={nodes.wheel_FL_2.geometry} material={materials.rims} />
+            </group>
+            <group ref={wheels[1]}>
+              <mesh castShadow={true} geometry={nodes.wheel_FR_1.geometry} material={materials.tire} />
+              <mesh castShadow={true} geometry={nodes.wheel_FR_2.geometry} material={materials.rims} />
+            </group>
+            <group ref={wheels[2]} >
+              <mesh castShadow={true} geometry={nodes.wheel_RL_1.geometry} material={materials.tire} />
+              <mesh castShadow={true} geometry={nodes.wheel_RL_2.geometry} material={materials.rims} />
+            </group>
+            <group ref={wheels[3]} >
+              <mesh castShadow={true} geometry={nodes.wheel_RR_1.geometry} material={materials.tire} />
+              <mesh castShadow={true} geometry={nodes.wheel_RR_2.geometry} material={materials.rims} />
+            </group>
           </group>
-
-          <group>
-            <mesh castShadow={true} geometry={nodes.wheel_FR_1.geometry} material={materials.tire} />
-            <mesh castShadow={true} geometry={nodes.wheel_FR_2.geometry} material={materials.rims} />
-          </group>
-
-          <group>
-            <mesh castShadow={true} geometry={nodes.wheel_RL_1.geometry} material={materials.tire} />
-            <mesh castShadow={true} geometry={nodes.wheel_RL_2.geometry} material={materials.rims} />
-          </group>
-
-          <group>
-            <mesh castShadow={true} geometry={nodes.wheel_RR_1.geometry} material={materials.tire} />
-            <mesh castShadow={true} geometry={nodes.wheel_RR_2.geometry} material={materials.rims} />
-          </group>
-
         </group>
       </group>
-
     </group>
   </>
   )

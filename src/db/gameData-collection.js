e934f9fb9cc11784af4cfdadaa3925f0b265c6ba
Cristia Medina <cdmedina229@gmail.com>
2024-05-18 03:01:56 -0500

    db & finishLine

---
+++ src/db/gameData-collection.js
@@ -0,0 +1,66 @@
+"use strict";
+
+import { addDoc, collection, getDocs, query, where, updateDoc } from "firebase/firestore";
+import { db } from "../firebase.config";
+
+
+
+const gameDataRef = collection(db, "gameData");
+
+const createGameData = async (data) => {
+    try {
+        const exist = await readGameData(data.email);
+        if(!exist.success){
+            console.log("not exist")
+            await addDoc(gameDataRef, { 
+                userKey: data.email,
+                lastLevel: 0,
+                levelComplete: false,
+                points: 0,
+                time: 0,
+            });
+            return await readGameData(data.email);
+        }
+        return exist
+    } catch (error) {
+        return error;
+    }
+};
+
+const readGameData = async (userEmail) => {
+    try {
+        const userSnapshot = await getDocs(query(gameDataRef, where("userKey", "==", userEmail)));
+
+        if (userSnapshot.empty) {
+            return { success: false, message: "User not found" };
+        }
+
+        const userData = userSnapshot.docs.map((doc) => doc.data());
+        return { success: true, userData: userData[0] };
+    } catch (error) {
+        return error;
+    }
+};
+
+const updateGameData = async (userEmail, newData) => {
+    try {
+        
+        const userSnapshot = await getDocs(query(gameDataRef, where("userKey", "==", userEmail)));
+
+        if (userSnapshot.empty) {
+            return { success: false, message: "User not found" };
+        }
+        const userDoc = userSnapshot.docs[0];
+
+        await updateDoc(userDoc.ref, {
+            ...newData
+        });
+        const _newData = await readGameData(userEmail).userData;
+        return { success: true, userData: _newData,message: "gameData updated successfully" };
+    } catch (error) {
+        console.log('ocurrio un error', error)
+        return error;
+    }
+};
+
+export { createGameData, readGameData, updateGameData };

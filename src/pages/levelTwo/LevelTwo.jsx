ecefba4ffebe0a15db86dd77078a743e34fd0b36
Sebastian Rey <reysebastian77@gmail.com>
2024-06-02 16:05:21 -0500

    inclusion de hdr en nivel 3

--- src/pages/levelTwo/LevelTwo.jsx
+++ src/pages/levelTwo/LevelTwo.jsx
@@ -1,6 +1,6 @@
 import { Canvas } from "@react-three/fiber"
 import { LightsOne } from "../levelOne/lights/LightsOne"
-import { CameraControls, PerspectiveCamera } from "@react-three/drei"
+import { CameraControls, Environment, PerspectiveCamera } from "@react-three/drei"
 import React, { Suspense, useState } from 'react'
 import { Perf } from "r3f-perf"
 import { WorldTwo } from "./world/WorldTwo"
@@ -9,6 +9,7 @@ import { Debug, Physics } from '@react-three/cannon'
 import { CubeCar } from '../../components/cars/CubeCar'
 import { useCarContext } from '../../context'
 
+const URLENVIRONMENT2 = '/assets/textures/dikhololo_night_1k.hdr'
 
 export const LevelTwo = () => {
     const [thirdPerson, setThirdPerson] = useState(false);
@@ -16,29 +17,30 @@ export const LevelTwo = () => {
 
     const { car } = useCarContext()
     return (
-            <>
-                <Canvas
-                    camera={{
-                        fov: 45,
-                        near: 0.7,
-                        far: 300,
-                        position: [5, 4, 5]
-                    }}
-                >
-                    <Physics
+        <>
+            <Canvas
+                camera={{
+                    fov: 45,
+                    near: 0.7,
+                    far: 300,
+                    position: [5, 4, 5]
+                }}
+            >
+                <Environment files={URLENVIRONMENT2} background={"both"} />
+                <Physics
                     broadphase="SAP"
                     gravity={[0, -2.6, 0]}
-                    >
-                        <CubeCar thirdPerson={thirdPerson} />
-                        <PerspectiveCamera makeDefault position={[0, 10, 20]} />
-                        <CameraControls />
-                        <color attach="background" args={["#ececec"]} />
-                        <LightsTutorial />                
-                        <Perf />
-                        <WorldTwo />
-                    </Physics>
-                    
-                </Canvas>
-            </>
+                >
+                    <CubeCar thirdPerson={thirdPerson} />
+                    <PerspectiveCamera makeDefault position={[0, 10, 20]} />
+                    <CameraControls />
+                    <color attach="background" args={["#ececec"]} />
+                    <LightsTutorial />
+                    <Perf />
+                    <WorldTwo />
+                </Physics>
+
+            </Canvas>
+        </>
     )
 }

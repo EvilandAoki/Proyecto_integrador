8dd837600f44978f546e770c7a598e35f181c413
Sebastian Rey <reysebastian77@gmail.com>
2024-06-20 14:59:06 -0500

    implementacion de componente de esfera

--- src/pages/levelThree/world/WorldThree.jsx
+++ src/pages/levelThree/world/WorldThree.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useGLTF } from '@react-three/drei'
 import { RigidBody } from '@react-three/rapier'
 import { usePlane } from '@react-three/cannon';
+import { Ball } from '../../../components/ball/Ball';
 
 const Plane = ({ position, rotation, args, color }) => {
     const [ref] = usePlane(() => ({
@@ -23,6 +24,8 @@ export const WorldThree = (props) => {
     return (
         <>
             <Plane position={[0, -0.81, -30]} rotation={[-Math.PI / 2, 0, 0]} args={[100, 100]} color="grey" />
+            {/* Componente de esfera */}
+            <Ball />
         </>
     )
 }

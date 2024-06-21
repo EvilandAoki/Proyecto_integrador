4f301bcde0ab99f1198aad2daa0c5c436d3ab997
alejandro <alejandro.villamil@correounivalle.edu.co>
2024-06-20 19:10:33 -0500

    Mapa del nivel 3

--- src/pages/levelThree/world/WorldThree.jsx
+++ src/pages/levelThree/world/WorldThree.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useGLTF } from '@react-three/drei'
 import { RigidBody } from '@react-three/rapier'
 import { usePlane } from '@react-three/cannon';
+import { ColliderBox } from '../../../components/colliders/ColliderBox';
 import { Ball } from '../../../components/ball/Ball';
 
 const Plane = ({ position, rotation, args, color }) => {
@@ -23,9 +24,32 @@ const Plane = ({ position, rotation, args, color }) => {
 export const WorldThree = (props) => {
     return (
         <>
-            <Plane position={[0, -0.81, -30]} rotation={[-Math.PI / 2, 0, 0]} args={[100, 100]} color="grey" />
+            <Plane position={[0, -0.81, -0]} rotation={[-Math.PI / 2, 0, 0]} args={[22, 35]} color="green" />
             {/* Componente de esfera */}
             <Ball />
+            {/* Cancha */}
+            <ColliderBox position={[-10, -0.3, 0]} scale={[0.3, 1, 30]} color="black" />
+            <ColliderBox position={[10, -0.3, 0]} scale={[0.3, 1, 30]} color="black" />
+            <ColliderBox position={[-6.15, -0.3, -15.15]} scale={[8, 1, 0.3]} color="black" />
+            <ColliderBox position={[6.15, -0.3, -15.15]} scale={[8, 1, 0.3]} color="black" />
+            <ColliderBox position={[-6.15, -0.3, 15.15]} scale={[8, 1, 0.3]} color="black" />
+            <ColliderBox position={[6.15, -0.3, 15.15]} scale={[8, 1, 0.3]} color="black" />
+            {/* Lineas */}
+            <ColliderBox position={[0, -0.80, 0]} scale={[20, 0, 0.1]} color="white" />
+            <ColliderBox position={[0, -0.80, -9]} scale={[10, 0, 0.1]} color="white" />
+            <ColliderBox position={[0, -0.80, 9]} scale={[10, 0, 0.1]} color="white" />
+            <ColliderBox position={[-5, -0.80, -12]} scale={[0.1, 0, 6.1]} color="white" />
+            <ColliderBox position={[5, -0.80, -12]} scale={[0.1, 0, 6.1]} color="white" />
+            <ColliderBox position={[-5, -0.80, 12]} scale={[0.1, 0, 6.1]} color="white" />
+            <ColliderBox position={[5, -0.80, 12]} scale={[0.1, 0, 6.1]} color="white" />
+            {/* Arco 1 */}
+            <ColliderBox position={[2, -0.3, -16]} scale={[0.3, 1, 2]} color="blue" />
+            <ColliderBox position={[-2, -0.3, -16]} scale={[0.3, 1, 2]} color="blue" />
+            <ColliderBox position={[0, -0.3, -16.85]} scale={[3.7, 1, 0.3]} color="blue" />
+            {/* Arco 2 */}
+            <ColliderBox position={[2, -0.3, 16]} scale={[0.3, 1, 2]} color="blue" />
+            <ColliderBox position={[-2, -0.3, 16]} scale={[0.3, 1, 2]} color="blue" />
+            <ColliderBox position={[0, -0.3, 16.85]} scale={[3.7, 1, 0.3]} color="blue" />
         </>
     )
 }
